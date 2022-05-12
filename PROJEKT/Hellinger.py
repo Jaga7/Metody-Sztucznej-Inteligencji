c4636ad102f8e2cdc305531c9668c80c947cc1df
Andrzej J <75665204+Jaga7@users.noreply.github.com>
2022-05-12 12:27:15 +0200

    Działający Hellinger

--- PROJEKT/Hellinger.py
+++ PROJEKT/Hellinger.py
@@ -18,6 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
+
 # Feature selection: hellinger
 
 def hellinger(X, y):
@@ -30,23 +31,25 @@ def hellinger(X, y):
     valuesOfAttributes = np.zeros((2, X[0].shape[0], len(bin_edges_cale)-1))
     valuesOfAttributes = give_discretized_distributions(X, y)
     
-    list_of_distances=[]
+    array_of_distances=[]
 
-    list_of_squares = []
+    array_of_squares = []
 
     # iterowanie przez rozkłady dla atrybutów
     for idxAttribute, distributionsForClasses in enumerate(valuesOfAttributes[:,]):
-        list_of_squares = []
+        array_of_squares = []
         # iterowanie przez biny, indeks 0 tylko po to żeby dostać ilość binów
         for wartośćWBinieDlaKlasy0, wartośćWBinieDlaKlasy1 in zip(distributionsForClasses[0], distributionsForClasses[1]):
-            print(idxAttribute," a wartość w binie dla klasy 0: ", wartośćWBinieDlaKlasy0," a wartość w binie dla klasy 1: ", wartośćWBinieDlaKlasy1)
-            s = (math.sqrt(wartośćWBinieDlaKlasy0) - math.sqrt(wartośćWBinieDlaKlasy1)) ** 2
-            list_of_squares.append(s)
-            if(len(list_of_squares)==len(distributionsForClasses[0])):
-                sosq = sum(list_of_squares)
-                list_of_distances.append(sosq / math.sqrt(2))
+            calculation = (math.sqrt(wartośćWBinieDlaKlasy0) - math.sqrt(wartośćWBinieDlaKlasy1)) ** 2
+            array_of_squares.append(calculation)
+            if(len(array_of_squares)==len(distributionsForClasses[0])):
+                sum_of_squares = sum(array_of_squares)
+                array_of_distances.append(sum_of_squares / math.sqrt(2))
 
-    return list_of_distances
+    # we don't need to return p_values for SelectKBest method, so we return an array of zeros
+    WRONGp_values=np.zeros(len(array_of_distances))
+
+    return array_of_distances,WRONGp_values
 
 
 def give_discretized_distributions(X, y):
@@ -60,7 +63,6 @@ def give_discretized_distributions(X, y):
 
     # X[0] daje wartości atrybutów dla próbki o indeksie 0, bierzemy w celu przeiterowania przez ilość atrybutów
     for idxAttribute, uselessValue in enumerate(X[0]):
-        print('IDXATTRIBUTE',idxAttribute)
         # Służy do zapisania wartości dla różnych klas i zrobienia z nich histogramów
         valuesOfAttributesForClass0=[]
         valuesOfAttributesForClass1=[]
@@ -79,13 +81,34 @@ def give_discretized_distributions(X, y):
                 # zapisanie prawdopodobieństw występowań (w sensie normalized frequencies) dla rozkładów do macierzy
                 valuesOfAttributes[idxAttribute,0]=XClass0NormalizedFrequencies
                 valuesOfAttributes[idxAttribute,1]=XClass1NormalizedFrequencies
+
     return valuesOfAttributes
 
 # próba czy działa 
 dystanse=hellinger(X,y)
-print("PRÓBA: ",dystanse)
+print(dystanse)
+
+# dupa=give_discretized_distributions(X,y)
+# print(dupa.shape)
+# print(dupa[0,0])
+# print(dupa[0,1])
+# # iterowanie przez rozkłady dla atrybutów
+# for idxAttribute, distributionsForClasses in enumerate(dupa[:,]):
+#     # iterowanie przez biny, indeks 0 tylko po to żeby dostać ilość binów
+#     for wartośćWBinieDlaKlasy0, wartośćWBinieDlaKlasy1 in zip(distributionsForClasses[0], distributionsForClasses[1]):
+#         print(idxAttribute," a wartość w binie dla klasy 0: ", wartośćWBinieDlaKlasy0," a wartość w binie dla klasy 1: ", wartośćWBinieDlaKlasy1)
+    
+
+
+# #iterowanie przez wartości w binach atrybutu o indeksie 0 o klasie 0
+# for idxBin, value in enumerate(XAttribute1NormalizedFrequencies):
+#     if y[idxBin]==0:valuesOfAttributes[0,0,idxBin]
+#     # print(idxBin, value)o
+
 
 # print(valuesOfAttributes[0,0])
 # print(valuesOfAttributes[1,0])
 
 # print("CAŁA MACIERZ 3-WYMIAROWA: ",valuesOfAttributes.shape)
+
+# print(hellinger_explicit(XClass1NormalizedFrequencies,XClass2NormalizedFrequencies))
