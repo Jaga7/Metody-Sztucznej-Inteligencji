ef340e577366c49e1bb67b427e904ab91b51ff29
Andrzej J <75665204+Jaga7@users.noreply.github.com>
2022-05-12 09:35:14 +0200

    Wyrzucony niepotrzebny kod

--- PROJEKT/Hellinger.py
+++ PROJEKT/Hellinger.py
@@ -17,104 +17,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=.2,
 )
 
-# print(X_train.shape)
-# print(X.shape)
-# print(np.histogram(X))
-
-
-Xcale=np.histogram(X)
-
-
-# print("Całe X: ",np.histogram(X))
-
-# def hellinger_explicit(p, q):
-#     """Hellinger distance between two discrete distributions.
-#        Same as original version but without list comprehension
-#     """
-#     # p=np.histogram(p)
-#     # q=np.histogram(q)
-
-
-#     list_of_squares = []
-#     for p_i, q_i in zip(p, q,strict=True):
-
-#         # caluclate the square of the difference of ith distr elements
-#         s = (math.sqrt(p_i) - math.sqrt(q_i)) ** 2
-
-#         # append 
-#         list_of_squares.append(s)
-
-#     # calculate sum of squares
-#     sosq = sum(list_of_squares)    
-
-#     return sosq / math.sqrt(2)
-
-
-def hellinger_explicit(X, y):
-    """Hellinger distance between two discrete distributions.
-       Same as original version but without list comprehension
-    """
-    # p=np.histogram(p)
-    # q=np.histogram(q)
-    
-
-    # To poniżej to raczej ma być pętla i takie coś dla każdego atrybutu
-    XHist,XBinEdges=np.histogram(X)
-    XClass1=np.histogram(X[0],XBinEdges)
-    XClass2=np.histogram(X[1],XBinEdges)
-    XClass1Hist,XClass1BinEdges=XClass1
-    XClass2Hist,XClass2BinEdges=XClass2
-    XClass1NormalizedFrequencies=XClass1Hist/sum(XClass1Hist)
-    XClass2NormalizedFrequencies=XClass2Hist/sum(XClass2Hist)
-
-    list_of_squares = []
-    for p_i, q_i in zip(XClass1NormalizedFrequencies, XClass2NormalizedFrequencies,strict=True):
-
-        # caluclate the square of the difference of ith distr elements
-        s = (math.sqrt(p_i) - math.sqrt(q_i)) ** 2
-
-        # append 
-        list_of_squares.append(s)
-
-    # calculate sum of squares
-    sosq = sum(list_of_squares)    
-
-    return sosq / math.sqrt(2)
-
-# to chyba było źle i nie usunąłem
-histCale, bin_edges_cale=Xcale
-XAttribute1=np.histogram(X[:,0],bin_edges_cale)
-XAttribute2=np.histogram(X[:,1],bin_edges_cale)
-
-# print("Dla XAttribute1",XAttribute1)
-# print("Dla XAttribute2",XAttribute2)
-
-XAttribute1Hist,XAttribute1BinEdges=XAttribute1
-XAttribute2Hist,XAttribute2BinEdges=XAttribute2
-XAttribute1NormalizedFrequencies=XAttribute1Hist/sum(XAttribute1Hist)
-XAttribute2NormalizedFrequencies=XAttribute2Hist/sum(XAttribute2Hist)
-# print(XAttribute1Hist)
-# print(sum(XAttribute1Hist))
-# print(XAttribute1P)
-
-# print(X[0].shape[0])
-
-# print(X[:,0])
-
-# for idxAttribute in range(0,X[0].shape[0]-1):
-#     for idxSample, value in enumerate(X[:,0]):
-#         print(idxSample, value)
-
-
-
-
-# stworzenie 3-wymiarowej macierzy, klasy, atrybuty, wartości w binach
-valuesOfAttributes = np.zeros((2, X[0].shape[0], len(bin_edges_cale)-1))
-
 # bin_edges_cale chyba będzie trzeba inne dla każdego atrybutu
 # Bierzemy bin_edges_cale (tu dla atrybutu o indeksie 0), wykorzystamy przy tworzeniu histogramów dla X+ i X-
 Xcale=np.histogram(X[:,0])
 histCale, bin_edges_cale=Xcale
+
+# stworzenie 3-wymiarowej macierzy, klasy, atrybuty, wartości w binach
+valuesOfAttributes = np.zeros((2, X[0].shape[0], len(bin_edges_cale)-1))
+
 
 # Służy do zapisania wartości dla różnych klas i zrobienia z nich histogramów
 valuesOfAttributesForClass0=[]
@@ -137,10 +47,6 @@ for idxSample, value in enumerate(X[:,0]):
         valuesOfAttributes[1,0]=XClass1NormalizedFrequencies
 
 
-#iterowanie przez wartości w binach atrybutu o indeksie 0 o klasie 0
-for idxBin, value in enumerate(XAttribute1NormalizedFrequencies):
-    if y[idxBin]==0:valuesOfAttributes[0,0,idxBin]
-    # print(idxBin, value)o
 
 
 print(valuesOfAttributes[0,0])
